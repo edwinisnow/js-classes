ce3f8ad2e497c4902256848555422b2229b8f754
EDWIN GEORGE LAZER <edwin04@gmail.com>
2023-08-22 06:33:20 +0100

    Code Setup, Classes, Constructors, Extends, Super

---
+++ README.md
@@ -0,0 +1,22 @@
+# Classes
+
+## Differences
+
+| Functions          | Classes              |
+| ------------------ | -------------------- |
+| Hoisted            | Not hoisted          |
+| Can be overwritten | Can't be overwritten |
+
+## Static Methods
+
+- These are utility methods that you can create for the class, but not possible to get actual instance of that class
+
+## Constructors
+
+- Constructors are part of the syntax of the class
+- Automatically generated
+- Only one allowed per class
+
+## Super Keyword
+
+- Allows you to call the method of the parent class
---
+++ app.js
@@ -0,0 +1,45 @@
+class Car {
+  constructor(doors, engine, color) {
+    this.doors = doors;
+    this.engine = engine;
+    this.color = color;
+  }
+
+  carStats() {
+    return `This car has ${this.doors} doors, ${this.engine} engine and a beutiful ${this.color} color, `;
+  }
+
+  static totalDoors(car1, car2) {
+    const doors1 = car1.doors;
+    const doors2 = car2.doors;
+    return doors1 + doors2;
+  }
+}
+
+const cx5 = new Car(4, 'V8', 'Grey');
+console.log(cx5);
+console.log(cx5.carStats());
+
+const civic = new Car(3, 'V4', 'Blue');
+console.log(civic);
+console.log(civic.carStats());
+
+//
+console.log(Car.totalDoors(cx5, civic));
+// extends
+class SUV extends Car {
+  constructor(doors, engine, color, brand, carStats) {
+    super(doors, engine, color, carStats);
+    this.brand = brand;
+    // default properties, no need to pass in
+    this.wheels = 4;
+    this.ac = true;
+  }
+  myBrand() {
+    return console.log(`This SUV is a ${this.brand}`);
+  }
+}
+const suv = new SUV(4, 'V8', 'Grey', 'mazda');
+console.log(suv);
+console.log(suv.carStats());
+console.log(suv.myBrand());
